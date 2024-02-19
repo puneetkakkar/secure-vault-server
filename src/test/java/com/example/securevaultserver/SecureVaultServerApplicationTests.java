07c6775b19e251000f01dc44e8bbdff164a7780f
Puneet Kakkar <puneetkakkar@Puneets-MBP-2.lan>
2024-02-19 00:52:08 -0500

    Initial commit

---
+++ src/test/java/com/example/securevaultserver/SecureVaultServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securevaultserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureVaultServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

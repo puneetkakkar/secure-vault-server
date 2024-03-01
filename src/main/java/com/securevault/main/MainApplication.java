2c879eed55308a8bc27fa8008d60e326d616419a
Puneet Kakkar <puneetkakkar91@gmail.com>
2024-02-29 19:44:56 -0500

    add spring web, and set live reloading

--- src/main/java/com/securevault/main/MainApplication.java
+++ src/main/java/com/securevault/main/MainApplication.java
@@ -2,12 +2,21 @@ package com.securevault.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MainApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MainApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "Java") String name) {
+		return String.format("Hello %s!", name);
 	}
 
 }

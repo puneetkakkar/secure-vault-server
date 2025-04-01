7364d2efc927f3e1163855ee9a3c87bae53ce895
Puneet Kakkar <puneetkakkar91@gmail.com>
2025-03-31 20:16:50 -0400

    fix and organize imports for modified files

--- src/main/java/com/securevault/main/dto/response/auth/TokenExpiresInResponse.java
+++ src/main/java/com/securevault/main/dto/response/auth/TokenExpiresInResponse.java
@@ -1,7 +1,5 @@
 package com.securevault.main.dto.response.auth;
 
-import com.securevault.main.dto.response.AbstractBaseResponse;
-
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
@@ -10,5 +8,5 @@ import lombok.experimental.SuperBuilder;
 @Setter
 @SuperBuilder
 public class TokenExpiresInResponse {
-    private Long token;
+	private Long token;
 }
